28783dbf4ea8fc4aafb9a6e5175da7d38b94aa63
koushikdev22 <koushik.journey.mondal@gmail.com>
2026-02-23 15:42:21 +0530

    new

---
+++ StringReplace.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StringReplace {
+
+    public String hideMessage(String message) {
+        List<String> notToChange = Arrays.asList("the", "of", "is", "to", "a", "an", "in", "it", "and");
+        String[] count = message.split(" ");
+        StringBuilder result = new StringBuilder();
+        return "hi";
+    }
+    public static void main(String Args[]){
+        String input = "The purpose of this exercise is to test logical thinking.";
+    }
+}
